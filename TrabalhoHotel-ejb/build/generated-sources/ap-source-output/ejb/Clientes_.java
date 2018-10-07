56715bf533d293ef2f874cbebf00c296a713ab7d
ggditzel <ggditzel@gmail.com>
2018-10-07 15:42:57 -0300

    Consegue listar os clientes

--- TrabalhoHotel-ejb/build/generated-sources/ap-source-output/ejb/Clientes_.java
+++ TrabalhoHotel-ejb/build/generated-sources/ap-source-output/ejb/Clientes_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-10-07T15:23:54")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-10-07T15:40:29")
 @StaticMetamodel(Clientes.class)
 public class Clientes_ { 
 
